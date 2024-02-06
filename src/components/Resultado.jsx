f13eb36adff2c6fbc498179a92d54ea5c2bb4f45
diegomehats <diegomehats@gmail.com>
2024-02-06 19:46:37 -0300

    subiendo proyeto completo

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,46 @@
+import useCotizador from "../hooks/useCotizador"
+import {MARCAS, PLANES} from '../constants'
+import { useCallback, useRef } from "react"
+
+
+
+const Resultado = () => {
+    const {resultado, datos} = useCotizador()
+    const {marca, plan, year} = datos
+    const [nombreMarca] = useCallback( MARCAS.filter(m => m.id === Number(marca)), [resultado])
+    const [nombrePlan] = useCallback( PLANES.filter(p => p.id === Number(plan)), [resultado])
+    const yearRef = useRef(year)
+
+    if (resultado === 0) return null
+  return (
+
+    <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+      <h2 className="text-gray-600 font-black text-3xl"> Resumen </h2>
+      <p className=" my-2">
+        <span className="font-bold">Marca: </span>
+        {nombreMarca.nombre}
+      </p>
+
+      <p className=" my-2">
+        <span className="font-bold">Plan: </span>
+        {nombrePlan.nombre}
+      </p>
+
+      <p className=" my-2">
+        <span className="font-bold">Anio del auto: </span>
+        {yearRef.current}
+      </p>
+
+      <p className=" my-2 text-2xl">
+        <span className="font-bold">Total cotizacion: </span>
+        {resultado}
+      </p>
+
+
+
+
+    </div>
+  )
+}
+
+export default Resultado
